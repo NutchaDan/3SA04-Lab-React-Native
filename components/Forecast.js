d4b90963ea0767edaaadf3b20dc3c94dcddf43cb
NutchaDan <nutcha.siriwut@gmail.com>
2022-08-27 00:47:59 +0700

    part 3 Flex Box

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Forecast(props) {
     return (
-        <View>
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+        <View style={styles.view}>
+            <Text style={styles.fontmain}>{props.main}</Text>
+            <Text style={styles.fontdescription}>{props.description}</Text>
+            <Text style={styles.fonttemp}>{props.temp} °C</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    fontmain: {
+        color: '#5A6E3A',
+        fontSize: '35px',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        paddingBottom: '20px',
+        paddingTop: '15px'
+    },
+    fontdescription: {
+        color: '#5A6C16',
+        fontSize: '18px',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        paddingBottom: '30px'
+    },
+    fonttemp: {
+        color: '#667761',
+        fontSize: '25px',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    }
+})
--- components/Weather.js
+++ components/Weather.js
@@ -4,15 +4,14 @@ import Forecast from "./Forecast";
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-        main:  '-',
-        description: '-',
+        main:  'main',
+        description: 'description',
         temp: 0
     })
 
     return (
-        <ImageBackground source={require('../background.jpg')} style={styles.backdrop}>
-            <Text>Zip Code</Text>
-            <Text>{props.zipCode}</Text>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text style={styles.stylefont}>Zip Code is {props.zipCode}</Text>
             <Forecast {...forecastInfo}/>
         </ImageBackground>
     )
@@ -20,8 +19,16 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop: {
+        flexDirection: 'column',
         alignItems: 'center',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        paddingTop: '25px',
+    },
+    stylefont: {
+        color: '#2E3D50',
+        fontSize: '20px',
+        textAlign: 'center',
+        textAlignVertical: 'center'
     }
 })
